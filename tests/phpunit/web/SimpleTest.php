bdd3c5df0b7ded01bce335a7afafed785efad2a3
Flyingmana <flyingmana@googlemail.com>
2014-06-15 00:11:36 +0200

    add example tests for category and product page

--- tests/phpunit/web/SimpleTest.php
+++ tests/phpunit/web/SimpleTest.php
@@ -46,5 +46,71 @@ class SimpleTest extends WebTestCase {
 
     }
 
+
+
+    /**
+     *
+     */
+    public function testCatalogCategory()
+    {
+        $request = new Request();
+        $request->setRequestUri('/electronics/cameras.html');
+
+        $response = self::executeWebRequest( $request );
+        $this->assertContains(
+            '<h1>Cameras</h1>',
+            trim($response->getSendResponseContent())
+        );
+
+    }
+
+    /**
+     *
+     */
+    public function testCatalogCategory2()
+    {
+        $request = new Request();
+        $request->setRequestUri('/apparel/hoodies.html');
+
+        $response = self::executeWebRequest( $request );
+        $this->assertContains(
+            '<h1>Hoodies</h1>',
+            trim($response->getSendResponseContent())
+        );
+
+    }
+
+    /**
+     *
+     */
+    public function testCatalogProduct()
+    {
+        $request = new Request();
+        $request->setRequestUri('/argus-qc-2185-quick-click-5mp-digital-camera.html');
+
+        $response = self::executeWebRequest( $request );
+        $this->assertContains(
+            '<h1> Argus QC-2185 Quick Click 5MP Digital Camera</h1>',
+            trim($response->getSendResponseContent())
+        );
+
+    }
+
+    /**
+     *
+     */
+    public function testCatalogProduct2()
+    {
+        $request = new Request();
+        $request->setRequestUri('/apparel/hoodies/the-get-up-kids-band-camp-pullover-hoodie.html');
+
+        $response = self::executeWebRequest( $request );
+        $this->assertContains(
+            '<h1>The Get Up Kids: Band Camp Pullover Hoodie</h1>',
+            trim($response->getSendResponseContent())
+        );
+
+    }
+
 }
  
